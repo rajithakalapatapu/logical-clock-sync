1ff9f0b76d745cd1708271bd01f6eb4aab3529e1
rajitha <rajithakalapatapu9@gmail.com>
2019-02-14 22:23:54 -0600

    update client labels when a new client joins

--- server-main.py
+++ server-main.py
@@ -2,30 +2,50 @@ import tkinter as tk
 from tkinter import scrolledtext
 from tkinter import ttk
 import socket
-
+from threading import Thread
 
 # references: Python GUI cookbook
 server_window = tk.Tk()
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_host = "127.0.0.1"
 server_port = 9999
+connected_clients = []
+client_labels = [None, None, None]
+scr = None
 
 
-def main():
-    setup_server_window()
-    # TODO: server_window.mainloop()
-    setup_server_socket()
+def client_thread(client_sock, client_address, client_name):
+    print("Started client handling thread for client {}".format(client_name))
+
+
+def wait_for_new_clients():
+    while True:
+        server_socket.listen()
+        print("Waiting for client request..")
+        client_sock, client_address = server_socket.accept()
+        data_on_sock = client_sock.recv(2048)
+        data_on_sock = data_on_sock.decode()
+        client_name = data_on_sock.split(":")[1]
+        print(
+            "Client {} connected with name{}".format(
+                (client_sock, client_address), client_name
+            )
+        )
+        global connected_clients
+        connected_clients.append((client_sock, client_address, client_name))
+        print("List of all connected_clients {}".format(connected_clients))
+        client_labels[len(connected_clients) - 1]["text"] = client_name.strip()
+        t = Thread(target=(client_thread(client_sock, client_address, client_name)))
+        t.start()
 
 
 def setup_server_socket():
-    server_socket.bind((server_host, server_port))
-    server_socket.listen(1)
-    print("Server started")
-    print("Waiting for client request..")
-    clientsock, clientAddress = server_socket.accept()
-    data = clientsock.recv(2048)
-    data = data.decode()
-    print("Client connected {}".format(data))
+    try:
+        server_socket.bind((server_host, server_port))
+        print("Server started")
+    except OSError as e:
+        print("An error occurred - please fix before relaunching " + str(e))
+        raise e
 
 
 def setup_server_window():
@@ -35,18 +55,28 @@ def setup_server_window():
 
     label_frame = ttk.LabelFrame(server_window, text="List of active clients")
     label_frame.grid(column=0, row=0, padx=320, pady=100)
-    ttk.Label(label_frame).grid(column=0, row=0)
-    ttk.Label(label_frame).grid(column=0, row=1)
-    ttk.Label(label_frame).grid(column=0, row=2)
+    global client_labels
+    for i in range(len(client_labels)):
+        client_labels[i] = ttk.Label(label_frame)
+        client_labels[i].grid(column=0, row=i)
     for child in label_frame.winfo_children():
         child.grid_configure(padx=8, pady=4)
 
     scroll_width = 70
     scroll_height = 10
+    global scr
     scr = scrolledtext.ScrolledText(
         server_window, width=scroll_width, height=scroll_height, wrap=tk.WORD
     )
     scr.grid(column=0, columnspan=20, padx=10, pady=10)
+
+
+def main():
+    setup_server_window()
+    setup_server_socket()
+    t = Thread(target=wait_for_new_clients)
+    t.start()
+    server_window.mainloop()
 
 
 if __name__ == "__main__":
